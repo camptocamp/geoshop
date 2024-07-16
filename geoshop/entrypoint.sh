82fa5e835599e1c96b283d7ae8b19014e3b32072
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-07-16 17:22:14 +0200

    Add geoshop support out of the box with demo data

---
+++ geoshop/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+cd /geoshop/geoshop-back
+
+if [[ "$( python3 manage.py showmigrations | grep '\[ \]' )" != "" ]]; then
+    python3 manage.py migrate --noinput
+else
+    echo "No pending migrations"
+fi
+
+if [[ ! -f "/geoshop/.fixtures_lock" ]]; then
+    python3 manage.py fixturize
+    if [[ $? == 0 ]]; then 
+      touch "/geoshop/.fixtures_lock" 
+    fi
+else
+    echo "No fixturizing needed."
+fi
+
+if [[ ! -f "/geoshop/.sampledata_lock" ]]; then
+    # TODO: Pass host, password and dbname from the env section of the postgis
+    PGPASSWORD=geoshop psql --host=postgis --username=geoshop --dbname=geoshop -p 5432 < /geoshop/sampledata.sql
+    touch "/geoshop/.sampledata_lock"
+fi
+
+python3 manage.py collectstatic --noinput
+python3 manage.py runserver 0.0.0.0:8000
