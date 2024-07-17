dee449026b57557ef9c2b88ee567ed7019807ca7
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-07-17 12:57:54 +0200

    Fixed connector's matching rule in the demo data

--- updatedb/003_system_config.sql
+++ updatedb/003_system_config.sql
@@ -4,14 +4,10 @@ UPDATE system SET value = 'openldap' WHERE key = 'ldap_servers';
 UPDATE system SET value = true WHERE key = 'ldap_on'; 
 UPDATE system SET value = 'STARTTLS' WHERE key = 'ldap_encryption_type';
 
-INSERT INTO processes VALUES (4, 'Ask operator');
-INSERT INTO processes_users VALUES (4, 2), (4, 3);
+INSERT INTO public.processes (id_process, name) VALUES (4, 'Archive files');
+INSERT INTO public.processes_users (id_process, id_user) VALUES (4, 2);
+INSERT INTO public.processes_users (id_process, id_user) VALUES (4, 3);
 
-INSERT INTO connectors (id_connector, active, connector_code, connector_label, connector_params, import_freq, max_retries, name)
-VALUES (0, true, 'geoshopextract', 'Plugin Geoshop STIN', '{"uploadSize":"","detailsUrl":"","pass":"5%G*1jA^BpJDiEpi","login":"extract","url":"http://geoshop:8000"}', 4, 3, 'Extract')
-ON CONFLICT DO NOTHING;
-
-INSERT INTO tasks(id_task, task_code, task_label, task_params, position, id_process)
-VALUES(4, 'VALIDATION', 'Validation opérateur', '{"reject_msgs":"","valid_msgs":""}', 1, 4);
-
-INSERT INTO rules VALUES (4, true, 1, '*', 0, 4);
+INSERT INTO connectors (id_connector, active, connector_code, connector_label, connector_params, error_count, import_freq, last_import_date, last_import_msg, max_retries, name) VALUES (4, true, 'geoshopextract', 'Plugin Geoshop SITN', '{"uploadSize":"","detailsUrl":"","pass":"5%G*1jA^BpJDiEpi","login":"extract","url":"http://geoshop:8000"}', 0, 5, '2024-07-17 10:25:13.03', '', 3, 'Demo extract connector');
+INSERT INTO public.tasks (id_task, task_code, task_label, task_params, "position", id_process) VALUES (7, 'ARCHIVE', 'Archivage fichiers', '{"path":"/tmp/archive.zip"}', 1, 4);
+INSERT INTO public.rules (id_rule, active, "position", rule, id_connector, id_process) VALUES (4, true, 1, 'surface > 0', 4, 4);
