8d72f11572553e1123cc94253a48b7be99354f06
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-07-19 16:35:29 +0200

    Added dummy result file to check the workflow

--- extract/extract-task-exec/src/main/java/ch/asit_asso/extract/plugins/exec/ExecRequest.java
+++ extract/extract-task-exec/src/main/java/ch/asit_asso/extract/plugins/exec/ExecRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 arx iT
+ * Copyright (C) 2024
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- extract/extract-task-exec/src/main/java/ch/asit_asso/extract/plugins/exec/PluginConfiguration.java
+++ extract/extract-task-exec/src/main/java/ch/asit_asso/extract/plugins/exec/PluginConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 arx iT
+ * Copyright (C) 2024
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,6 @@ import org.slf4j.LoggerFactory;
 /**
  * Access to the settings for the archiving plugin.
  *
- * @author Florent Krin
  */
 public class PluginConfiguration {
 
