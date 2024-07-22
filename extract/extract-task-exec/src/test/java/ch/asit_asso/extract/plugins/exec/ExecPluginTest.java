5bcc69b6e8d47b814aef66754461944e169410d6
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-07-22 12:00:12 +0200

    Add a sample test for the task

---
+++ extract/extract-task-exec/src/test/java/ch/asit_asso/extract/plugins/exec/ExecPluginTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.asit_asso.extract.plugins.exec;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ *
+ * @author arusakov
+ */
+@Tag("fast")
+public class ExecPluginTest {
+   
+    @BeforeEach
+    public void setUp() {
+    
+    }
+    
+    @Test
+    public void testDocsLoaded() {
+        assertNotEquals("", new ExecPlugin().getHelp());
+    }
+}
