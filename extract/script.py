a23ac16a253121f4db55531ead2cf17f1b61289a
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-07-29 15:28:05 +0200

    Added geopandas support for python scripts

--- extract/script.py
+++ extract/script.py
@@ -1,21 +1,32 @@
 import json
+import os
 import sys
 from osgeo import ogr
 from os import path
+import psycopg2
+import geopandas
 
 src = sys.argv[1]
 out = sys.argv[2]
 
-srcData = {}
+conn = psycopg2.connect(database=os.environ["GEODATA_POSTGRES_DB"],
+                        host=os.environ["GEODATA_POSTGRES_HOST"],
+                        user=os.environ["GEODATA_POSTGRES_USER"],
+                        password=os.environ["GEODATA_POSTGRES_PASSWORD"])
+dbdf = geopandas.read_postgis("SELECT * FROM polygons", conn)
+conn.close()
+
+requestdf = None
 with open(path.join(src, "request.json")) as f:
     srcData = json.loads(f.read())
+    perimeter = srcData.get('perimeter')
+    if perimeter:
+        requestdf = geopandas.GeoSeries.from_wkt([perimeter])
 
 with open(path.join(out, "output.json"), "w+") as f:
-    perimeter = srcData.get('perimeter') or ""
-    if perimeter:
-        geomcol = ogr.CreateGeometryFromWkt(perimeter)
-        f.write("Geometry:\n")
-        for geom in range(0, geomcol.GetGeometryCount()):
-            f.write("\tPoints:\n")
-            for pt in geomcol.GetGeometryRef(geom).GetPoints():
-                f.write(f"\t\t{pt}\n")
+    f.write("RESULT\n")
+    f.write(f"DB: {dbdf}\n\n")
+    f.write(f"REQUEST: {requestdf}\n\n")
+    if dbdf is not None and requestdf is not None:
+        f.write(f"TOUCH_CHECK: \n")
+
