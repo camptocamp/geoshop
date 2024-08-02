718222a15984f8e10e0e09bf5bd134eddca31e61
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-08-02 12:29:44 +0200

    Make both extract and geoshop work from the one frontend

--- updatedb/update.sh
+++ updatedb/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-psql --host=$PGHOST --username=$PGUSER --dbname=$PGDB \
+psql --host=$PGHOST --username=$PGUSER --dbname=$PGDB  --set "geoshop_backend='$GEOSHOP_BACKEND'" \
   < /updatedb/001_update_db.sql  \
   < /updatedb/002_create_test_data.sql \
   < /updatedb/003_system_config.sql
