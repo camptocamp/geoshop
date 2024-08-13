b44673eb0e87848c7b823cc6deb17a84559cecc4
Marion <marion.baumgartner@camptocamp.com>
2024-08-13 11:30:54 +0200

    Add env variable through docker compoes, add sample data as a whole DB to avoid key error or unforessen issues

--- geoshop/back/entrypoint.sh
+++ geoshop/back/entrypoint.sh
@@ -18,12 +18,5 @@ else
     echo "No fixturizing needed."
 fi
 
-if [[ ! -f "/geoshop/.sampledata_lock" ]]; then
-    # TODO: Pass host, password and dbname from the env section of the postgis
-    # FIXME use enviromnet variales if possible
-    PGPASSWORD=geoshop psql --host=postgis --username=geoshop --dbname=geoshop -p 5432 < /geoshop/sampledata.sql
-    touch "/geoshop/.sampledata_lock"
-fi
-
 python3 manage.py collectstatic --noinput
 python3 manage.py runserver 0.0.0.0:8000
