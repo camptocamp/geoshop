bb7fa46f8f5531c654cbcae5d83cb32de07cc0e3
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2025-02-10 14:58:50 +0100

    Simplified end-to-end infrastructure

--- geoshop/back/resource/postgis/001_createtables.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION postgis;
-CREATE EXTENSION unaccent;
-CREATE EXTENSION "uuid-ossp";
-CREATE SCHEMA geoshop AUTHORIZATION geoshop;
-
--- Only if french is needed
-CREATE TEXT SEARCH CONFIGURATION fr (COPY = simple);
-ALTER TEXT SEARCH CONFIGURATION fr ALTER MAPPING FOR hword, hword_part, word
-WITH unaccent, simple;
