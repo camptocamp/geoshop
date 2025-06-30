14f8e1d395fb0eef9c4ff2280314196872f14204
Andrey Rusakov (andrey.rusakov@camptocamp.com) <lanseg@proton.me>
2025-06-30 14:22:19 +0200

    Improved README

--- extract/entrypoint.sh
+++ extract/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cacerts="/opt/java/openjdk/lib/security/cacerts"
+keystore="/cert/extract/keystore"
+mkdir -p /cert/extract/
+export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Djavax.net.ssl.trustStore=$keystore "
 
-keytool -delete -keystore -cacerts -storepass changeit -alias geoshop-ca
-keytool -import -trustcacerts -cacerts -storepass changeit -noprompt -alias geoshop-ca -file /cert/geoshop-ca.crt
+keytool -delete -keystore $keystore -storepass changeit -alias geoshop-ca | true
+keytool -import -trustcacerts -keystore $keystore -storepass changeit -noprompt -alias geoshop-ca -file /cert/geoshop-ca.crt
 
-keytool -delete -keystore -cacerts -storepass changeit -alias geoshop-back
-keytool -import -trustcacerts -cacerts -storepass changeit -noprompt -alias geoshop-back -file /cert/geoshop-back.crt | 0
+keytool -delete -keystore -keystore $keystore -storepass changeit -alias geoshop-back | true
+keytool -import -trustcacerts -keystore $keystore -storepass changeit -noprompt -alias geoshop-back -file /cert/geoshop-back.crt | 0
 
-keytool -delete -keystore -cacerts -storepass changeit -alias geoshop-front
-keytool -import -trustcacerts -cacerts -storepass changeit -noprompt -alias geoshop-front -file /cert/geoshop-back.crt | 0
+keytool -delete -keystore -keystore $keystore -storepass changeit -alias geoshop-front | true
+keytool -import -trustcacerts -keystore $keystore -storepass changeit -noprompt -alias geoshop-front -file /cert/geoshop-back.crt | 0
 
 catalina.sh run
